b471de7efd0795d7928c0e490b1a46193cb4f5df
alex-w-dev <yampi.ru@gmail.com>
2019-10-27 23:14:23 +0500

    make chrome screenshots

--- common/models/youtube/static/watch-video.js
+++ common/models/youtube/static/watch-video.js
@@ -3,27 +3,37 @@
 // const errors = require('loopback-common-errors');
 const CDP = require('chrome-remote-interface');
 const fs = require('fs');
-console.log(1, '1');
+const path = require('path');
+const cp = require('child_process');
+const detect = require('detect-port');
+const CHROME_PORT = 9222;
+
 module.exports = function(Youtube) {
   Youtube.watchVideo = function(ctx, youtubeVideoId) {
     return (async () => {
-      console.log(4444, '4444');
-      // CDP(async (client) => {
-      //   const { Page } = client;
-      //   try {
-      //     await Page.enable();
-      //     await Page.navigate({ url: 'https://youtube.com' });
-      //     await Page.loadEventFired();
-      //     const { data } = await Page.captureScreenshot();
-      //     fs.writeFileSync('scrot.png', Buffer.from(data, 'base64'));
-      //   } catch (err) {
-      //     console.error(err);
-      //   } finally {
-      //     await client.close();
-      //   }
-      // }).on('error', (err) => {
-      //   console.error(err);
-      // });
+      if (await detect(CHROME_PORT) === CHROME_PORT) {
+        cp.exec(`C:/dev/chrlauncher-win64-stable-codecs-sync/bin/chrome.exe --remote-debugging-port=${CHROME_PORT}`);
+      }
+
+      CDP(async (client) => {
+        const { Page } = client;
+        try {
+          await Page.enable();
+          await Page.navigate({ url: 'https://youtube.com' });
+          await Page.loadEventFired();
+          await new Promise(res => setTimeout(res, 2000));
+          const { data } = await Page.captureScreenshot();
+          fs.writeFileSync(path.join(__dirname, '../../../../screenshots', 'scrot.png'), Buffer.from(data, 'base64'));
+        } catch (err) {
+          console.error(err);
+        } finally {
+          await client.close();
+        }
+      }).on('error', (err) => {
+        console.error(err);
+      });
+
+      return 'OK';
     })();
   };
 
